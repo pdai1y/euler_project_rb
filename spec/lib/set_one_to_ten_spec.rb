d79ad84c3ad35ff95cc05b233a3780ccfa5336f2
Pat <pat.irwin4@gmail.com>
2019-04-02 23:59:59 -0400

    Finish problem 2. Begin problem 3, add prime array generator & prime checker

--- spec/lib/set_one_to_ten_spec.rb
+++ spec/lib/set_one_to_ten_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe EulerProject::SetOneToTen do
     end
   end
 
+  describe '#even_fibonacci_numbers' do
+    it 'should return 9227463' do
+      expect(ep.even_fibonacci_numbers).to eq(4613732)
+    end
+  end
+
+  describe '#largest_prime_factor' do
+    xit 'should return 29 with 13195' do
+      expect(ep.largest_prime_factor(13195, 15)).to eq(29)
+    end
+
+    xit 'should return # with 600851475143' do
+      expect(ep.largest_prime_factor(600851475143, 1000)).to eq()
+    end
+  end
 end
